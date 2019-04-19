9ad6003efd0ae2e0499143cf717d0da44d83b9f0
Bart van Duinkerken <bart.vanduinkerken@gmail.com>
2019-04-19 15:55:43 +0200

    Fixed some GitHub alerts

--- PasswordChangeComponent/src/app/components/custom/password-editor/password-editor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PasswordEditorDirective } from './password-editor.directive';
-
-describe('PasswordEditorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PasswordEditorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
