310e83309889656da24e195d577a0ef44bacbd5d
Bart van Duinkerken <bart.vanduinkerken@gmail.com>
2019-04-23 14:04:09 +0200

    Added unit testing of multiple files

--- PasswordChangeComponent/src/app/components/custom/password-editor/password-editor.component.spec.ts
+++ PasswordChangeComponent/src/app/components/custom/password-editor/password-editor.component.spec.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 
 import { PasswordEditorComponent } from './password-editor.component';
 import { PasswordEditorModel } from './password-editor.models';
+import { By } from '@angular/platform-browser';
 
 describe('PasswordEditorComponent', () => {
   let component: PasswordEditorComponent;
@@ -25,5 +26,20 @@ describe('PasswordEditorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain current password input', () => {
+    const field2Model = fixture.debugElement.query(By.css('input[name=currentPassword]')).references['currentPassword'];
+    expect(field2Model).toBeTruthy();
+  });
+
+  it('should contain new password input', () => {
+    const field2Model = fixture.debugElement.query(By.css('input[name=newPassword]')).references['newPassword'];
+    expect(field2Model).toBeTruthy();
+  });
+
+  it('should contain new confirm password input', () => {
+    const field2Model = fixture.debugElement.query(By.css('input[name=newPasswordConfirm]')).references['newPasswordConfirm'];
+    expect(field2Model).toBeTruthy();
   });
 });
